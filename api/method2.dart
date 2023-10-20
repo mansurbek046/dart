d5f30358e3ce733000260402ed6cb100001b775b
mansurbek046@gmail.com <mansurbek046@gmail.com>
2023-10-20 06:49:01 +0500

    added async

--- api/method2.dart
+++ api/method2.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 //without callback using async and await
-Future<String> getData() await {
+Future<String> getData() async {
 	var res=await http.get(Uri.parse("https://coffee.alexflipnote.dev/random.json"));
 	return res.toString();
 }
--- api/nethod3.dart
+++ api/nethod3.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 //without callback using async and await
 //with try catch
-Future<String>getData() await {
+Future<String>getData() async {
 	try {
 		var res=await http.get(Uri.parse("https://coffee.alexflipnote.dev/random.json"));
   	return res.toString();
