6382edccbbc31afd7b622fc462736de9d0863d2b
mansurbek046@gmail.com <mansurbek046@gmail.com>
2023-10-20 06:46:07 +0500

    mistakes are solved

--- api/method2.dart
+++ api/method2.dart
@@ -1,10 +1,11 @@
 import 'package:http/http.dart' as http;
 
 //without callback using async and await
-void main() async {
-  Future<String> getData() await {
+Future<String> getData() await {
 	var res=await http.get(Uri.parse("https://coffee.alexflipnote.dev/random.json"));
 	return res.toString();
-  }
+}
+
+void main() async {
   print(await getData());
 }
--- api/nethod3.dart
+++ api/nethod3.dart
@@ -1,16 +1,15 @@
 import 'package:http/http.dart' as http;
 
 //without callback using async and await
-void main() async {
-	//with try catch
-  Future<String>getData() await {
-  	try {
-  	  var res=await http.get(Uri.parse("https://coffee.alexflipnote.dev/random.json"));
-  	  return res.toString();
-  	} catch (e) {
-  		return e;
-  	}
+//with try catch
+Future<String>getData() await {
+	try {
+		var res=await http.get(Uri.parse("https://coffee.alexflipnote.dev/random.json"));
+  	return res.toString();
+  } catch (e) {
+  	return e;
   }
-  
+}
+void main() async {
   print(await getData());
 }
